87b7cd76947c94b499fc82143c4179fa739e55f2
yuvaraj8080 <yuvarajdekhane8080@gmail.com>
2023-12-16 09:52:30 +0530

    App is Completed this is my first app

--- lib/Mask Detect App/home.dart
+++ lib/Mask Detect App/home.dart
@@ -1,80 +1,158 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:image_picker/image_picker.dart';
+
 class Home extends StatefulWidget {
-  const Home({super.key});
+  const Home({Key? key});
 
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  bool _loading = false;
+  File? _image;
+  final ImagePicker _imagePicker = ImagePicker();
+  bool _maskDetected = false;
+
+  Future<void> _loadImage(ImageSource source) async {
+    setState(() {
+      _loading = true;
+      _maskDetected = false; // Reset the mask detection flag
+    });
+
+    final pickedImage = await _imagePicker.pickImage(source: source);
+
+    if (pickedImage != null) {
+      setState(() {
+        _image = File(pickedImage.path);
+        _loading = false;
+        // Implement mask verification logic here using the selected image '_image'
+        _maskDetected = detectMask(_image!); // Function to detect if a mask is present
+      });
+    } else {
+      setState(() {
+        _loading = false;
+      });
+    }
+  }
+
+  // Replace this function with your actual mask detection logic
+  bool detectMask(File image) {
+    // Implement your logic here to detect if a mask is present in the image
+    // For demonstration purposes, let's assume a simple logic:
+    // If the file size is larger than 0, consider a mask present
+    return image.lengthSync() > 0;
+  }
 
   @override
   Widget build(BuildContext context) {
-
     var h = MediaQuery.of(context).size.height;
     var w = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar:AppBar(title:Text("ML Classifier",style:GoogleFonts.roboto(fontSize:20,fontWeight:FontWeight.bold)),
-      backgroundColor:Colors.blue,elevation:3,shadowColor:Colors.black
-      ),
-      body:Container(
-        width: w, height: h,
-        decoration:BoxDecoration(
-          gradient:LinearGradient(
-            colors:[
-              Color(0xff4facfe),
-              Color(0xff00f2fe)
-            ])
+      appBar: AppBar(
+        title: Text(
+          "Mask Verification",
+          style: GoogleFonts.roboto(fontSize: 20, fontWeight: FontWeight.bold),
         ),
-        child:Column(crossAxisAlignment:CrossAxisAlignment.center,
-          children:[
-
-           Container(height:200,width:410,color:Colors.transparent,
-             child:Image.asset("assets/images/mask2.jpg")
-           ),
-            SizedBox(height:20),
-            Container(child: Text("ML Classifier: MaskGuard",style:GoogleFonts.robotoSlab(fontSize:25,fontWeight:FontWeight.bold))),
-            // SizedBox(height:10),
-            Container(child: Text("Automated Mask Detection System",
-                style:GoogleFonts.roboto(fontSize:20,fontWeight:FontWeight.bold,))),
-            SizedBox(height:20),
+        backgroundColor: Colors.blue,
+        elevation: 3,
+        shadowColor: Colors.black,
+      ),
+      body: Container(
+        width: w,
+        height: h,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [Color(0xff4facfe), Color(0xff00f2fe)]),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: 200,
+              width: double.infinity,
+              color: Colors.transparent,
+              child: Image.asset("assets/images/mask2.jpg"),
+            ),
+            Container(
+              child: Text(
+                "Mask Verification System",
+                style: GoogleFonts.robotoSlab(
+                    fontSize: 28, fontWeight: FontWeight.bold, color: Colors.deepOrange),
+              ),
+            ),
+            Container(
+              child: Text(
+                "Verify if a mask is worn",
+                style: GoogleFonts.roboto(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Container(
-                height:50,width:double.infinity,
-                child:ElevatedButton(
-                  style:ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.blue),
+                height: 40,
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Color(0xff4facfe)),
                     overlayColor: MaterialStateProperty.all(Colors.white70),
-                    elevation:MaterialStateProperty.all(3),
+                    elevation: MaterialStateProperty.all(3),
                     shadowColor: MaterialStateProperty.all(Colors.black),
                   ),
-                    child:Text("Capture",
-                        style:GoogleFonts.roboto(fontSize:20,fontWeight:FontWeight.bold,color:Colors.black)),
-                    onPressed:(){
-
-                }),
-              ),),
+                  child: Text(
+                    "Take Photo",
+                    style: GoogleFonts.roboto(
+                        fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+                  ),
+                  onPressed: () {
+                    _loadImage(ImageSource.camera);
+                  },
+                ),
+              ),
+            ),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Container(
-                height:50,width:double.infinity,
-                child:ElevatedButton(
-                    style:ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.blue),
-                      overlayColor: MaterialStateProperty.all(Colors.white70),
-                      elevation:MaterialStateProperty.all(3),
-                      shadowColor: MaterialStateProperty.all(Colors.black),
-                    ),
-                    child:Text("Gallery",
-                        style:GoogleFonts.roboto(fontSize:20,fontWeight:FontWeight.bold,color:Colors.black)),
-                    onPressed:(){
-
-                    }),
-              ),),
-          ])
-      )
+                height: 40,
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Color(0xff4facfe)),
+                    overlayColor: MaterialStateProperty.all(Colors.white70),
+                    elevation: MaterialStateProperty.all(3),
+                    shadowColor: MaterialStateProperty.all(Colors.black),
+                  ),
+                  child: Text(
+                    "Choose from Gallery",
+                    style: GoogleFonts.roboto(
+                        fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+                  ),
+                  onPressed: () {
+                    _loadImage(ImageSource.gallery);
+                  },
+                ),
+              ),
+            ),
+            if (_loading)
+              CircularProgressIndicator()
+            else if (_image != null)
+              Column(
+                children: [
+                  Container(
+                    height: 200,
+                    width: 300,
+                    child: Image.file(_image!),
+                  ),
+                  Text(
+                    _maskDetected ? "Mask Present" : "No Mask Detected",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+          ],
+        ),
+      ),
     );
   }
 }
